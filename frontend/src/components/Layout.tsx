d0c9eaee820b010672df513c287453eeefcb6bf3
basar <basarsubasi@protonmail.com>
2025-10-05 15:21:03 +0300

    layout

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -11,7 +11,7 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
     <div className="layout">
       {/* Header */}
       <header className="page-header">
-  <Link to="/" className="main-title">basarsubasi's photos</Link>
+  <Link to="/" className="main-title">basarsubasi's <span className="title-photos">photos</span></Link>
   <Link to="/bw-photos">b&w</Link>
   <Link to="/color-photos">colored</Link>
 </header>
@@ -19,7 +19,9 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
       <div className="content">{children}</div>
       
       {/* Footer */}
-      <footer className="page-footer">2025 © Başar Subaşı</footer>
+      <footer className="page-footer">
+        2025 © başar subaşı | <a href="https://github.com/basarsubasi/gallery" target="_blank" rel="noopener noreferrer">source code</a>
+      </footer>
     </div>
   );
 };
